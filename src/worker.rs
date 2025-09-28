82ac42c7a2c1339dce5b6e8d9c59c87107b6cd6c
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2025-09-28 12:58:04 +0800

    Support Windows NamedPipes

--- src/worker.rs
+++ src/worker.rs
@@ -1,8 +1,12 @@
 use async_trait::async_trait;
 use serde_json::Value;
+#[cfg(unix)]
+use tokio::net::UnixStream;
+#[cfg(windows)]
+use tokio::net::windows::named_pipe::ClientOptions;
 use tokio::{
     io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt},
-    net::{TcpStream, UnixStream},
+    net::TcpStream,
     sync::oneshot,
 };
 
@@ -33,9 +37,21 @@ pub async fn run_worker(
             println!("Client connected via TCP");
             Box::new(tcp)
         } else {
-            let unix = UnixStream::connect("/tmp/ipc_broker.sock").await?;
-            println!("Client connected via Unix socket");
-            Box::new(unix)
+            // Local IPC depending on OS
+            #[cfg(unix)]
+            {
+                let unix = UnixStream::connect("/tmp/ipc_broker.sock").await?;
+                println!("Client connected via Unix socket");
+                Box::new(unix)
+            }
+
+            #[cfg(windows)]
+            {
+                let pipe_name = r"\\.\pipe\ipc_broker";
+                let pipe = ClientOptions::new().open(pipe_name)?;
+                println!("Client connected via Named Pipe: {pipe_name}");
+                Box::new(pipe)
+            }
         };
     if let Some(tx) = ready_tx {
         let _ = tx.send(()); // signal bound & ready
