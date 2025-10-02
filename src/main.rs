73762575c7f06ec394f9c7991228f47928c21bea
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2025-10-02 10:10:43 +0800

    Add logs for package name and version

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,9 @@ use ipc_broker::broker;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let name = env!("CARGO_PKG_NAME");
+    let version = env!("CARGO_PKG_VERSION");
+    println!("{name} has started v{version}...");
+
     broker::run_broker().await
 }
